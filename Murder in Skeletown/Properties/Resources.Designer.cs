1f921088cb8db2668881d4bddeeb6c4ac579926a
Sam Neus <sneus@nd.edu>
2021-07-12 18:24:36 -0400

    Began incorporating images for npcs and backgrounds

--- Murder in Skeletown/Properties/Resources.Designer.cs
+++ Murder in Skeletown/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace Skeletown_Game.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Bouncer {
+            get {
+                object obj = ResourceManager.GetObject("Bouncer", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
